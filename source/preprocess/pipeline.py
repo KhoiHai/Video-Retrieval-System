a403f6aa92417177074d84dc5cacf73d6085ccc1
KhoiHai <lenhatkhoittmt2005@gmail.com>
2026-07-30 22:19:04 +0700

    [test]: Add debugging code line to test and measure keyframe extraction in Kaggle using GPU

--- source/preprocess/pipeline.py
+++ source/preprocess/pipeline.py
@@ -14,19 +14,35 @@ from source.storage.media.local_media_storage import LocalMediaStorage
 from source.models.TransNetV2.inference import TransNetV2
 from source.models.CLIP.clip import CLIP
 
+# Stuff
+import time
+
 class PreprocessingPipeline:
 
-    def __init__(self):
+    def __init__(self, output_dir):
         self.scene_detector = SceneDetector(model = TransNetV2(), threshold = 0.75)
         self.candidate_extractor = CandidateExtractor(blur_threshold = 110, duplicate_threshold = 0.7)
         self.keyframe_extractor = KeyframeExtractor(clip_model = CLIP(), similarity_threshold = 0.83, max_frames_per_segment = 6)
-        self.media_storage = LocalMediaStorage("data/keyframe")
+        self.output_dir = output_dir
+        self.media_storage = LocalMediaStorage(self.output_dir)
 
     def run(self, video_path):
-        video = Video(video_path = video_path)
+        video = Video(video_path=video_path)
+
+        start = time.perf_counter()
         self.scene_detector.detect(video)
+        print(f"[TIME] Scene Detection: {time.perf_counter() - start:.2f}s")
+
+        start = time.perf_counter()
         self.candidate_extractor.extract(video)
+        print(f"[TIME] Candidate Extraction: {time.perf_counter() - start:.2f}s")
+
+        start = time.perf_counter()
         self.keyframe_extractor.extract(video)
+        print(f"[TIME] Keyframe Extraction: {time.perf_counter() - start:.2f}s")
+
+        start = time.perf_counter()
         self.media_storage.save(video)
+        print(f"[TIME] Media Storage: {time.perf_counter() - start:.2f}s")
 
         return video
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from source.preprocess.pipeline import PreprocessingPipeline
 
-pipeline = PreprocessingPipeline()
+pipeline = PreprocessingPipeline("/kaggle/working/keyframes")
 
 video = pipeline.run(
-    "data/video/Video_01.mp4"
+    "/kaggle/input/datasets/whaleeatu/pov-video/Video_01.mp4"
 )
 
+'''
 for scene in video.scenes:
     print(scene)
+'''
